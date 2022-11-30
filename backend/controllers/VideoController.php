a31ec458aabf047f5c8600089903498f39c85067
Marcelo Buzzeti <marcelobuzzetti@gmail.com>
2022-11-30 14:23:35 -0300

    remove code

--- backend/controllers/VideoController.php
+++ backend/controllers/VideoController.php
@@ -81,17 +81,10 @@ class VideoController extends Controller
         $model = new Video();
 
         $model->video = UploadedFile::getInstanceByName('video');
-
+       
         if ($this->request->isPost && $model->save()) {
                 return $this->redirect(['view', 'video_id' => $model->video_id]);
         }
-      /*   if ($this->request->isPost) {
-            if ($model->load($this->request->post()) && $model->save()) {
-                return $this->redirect(['view', 'video_id' => $model->video_id]);
-            }
-        } else {
-            $model->loadDefaultValues();
-        } */
 
         return $this->render('create', [
             'model' => $model,
