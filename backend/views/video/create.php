f858e161d5659586787fe5510315cecb3f904e5b
Marcelo Buzzeti <marcelobuzzetti@gmail.com>
2022-11-30 09:17:54 -0300

    remove _create render

--- backend/views/video/create.php
+++ backend/views/video/create.php
@@ -13,10 +13,6 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <!-- <?= $this->render('_form', [
-                'model' => $model,
-            ]) ?> -->
-
     <div class="d-flex flex-column justify-content-center align-items-center">
         <div class="upload-icon">
             <i class="fas fa-upload"></i>
