3ffd12d2bcbee1dbd85b8cc9dab5903c7a7a4b0a
Marcelo Buzzeti <marcelobuzzetti@gmail.com>
2022-12-08 10:51:48 -0300

    slash missing

--- common/models/Video.php
+++ common/models/Video.php
@@ -161,12 +161,12 @@ class Video extends \yii\db\ActiveRecord
 
     public function getVideoLink()
     {
-        return Yii::$app->params['frontendUrl'].'storage/videos/'.$this->video_id.'.mp4';
+        return Yii::$app->params['frontendUrl'].'/storage/videos/'.$this->video_id.'.mp4';
     }
 
     public function getThumbnailLink()
     {
-        return $this->has_thumbnail ? Yii::$app->params['frontendUrl'].'storage/thumbs/'.$this->video_id.'.jpg' : '';
+        return $this->has_thumbnail ? Yii::$app->params['frontendUrl'].'/storage/thumbs/'.$this->video_id.'.jpg' : '';
     }
 
     public function afterDelete()
