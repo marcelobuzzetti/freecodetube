c5c413d52000ecf62c488fa34ac3ed0dbc8cf3bb
Marcelo Buzzeti <marcelobuzzetti@gmail.com>
2022-12-08 10:02:09 -0300

    video list

--- frontend/views/video/_video_item.php
+++ frontend/views/video/_video_item.php
@@ -3,13 +3,17 @@
 /** @var $model \common\models\Video */
 ?>
 
-<div class="card" style="width: 18rem;">
-    <div class="ratio ratio-16x9 mb-3">
+<div class="card m-3" style="width: 14rem;">
+    <div class="ratio ratio-16x9">
         <video src="<?php echo $model->getVideoLink() ?>" title="YouTube video" allowfullscreen poster="<?php echo $model->getThumbnailLink() ?>"></video>
     </div>
-    <div class="card-body">
-        <h5 class="card-title"><?php echo $model->title ?></h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="card-body p-2">
+        <h6 class="card-title m-0"><?php echo $model->title ?></h6>
+        <p class="text-muted card-text m-0">
+            <?php echo $model->createdBy->username ?>
+        </p>
+        <p class="text-muted card-text m-0">
+            140 views . <?php echo Yii::$app->formatter->asRelativeTime($model->created_at) ?>
+        </p>
     </div>
 </div>
--- frontend/views/video/index.php
+++ frontend/views/video/index.php
@@ -7,6 +7,10 @@ use yii\widgets\ListView;
 
 <?php echo ListView::widget([
     'dataProvider' => $dataProvider,
-    'itemView' => '_video_item'
+    'itemView' => '_video_item',
+    'layout' => "<div class='d-flex flex-wrap'>{items}</div>{pager}",
+    'itemOptions' => [
+        'tag' => false,
+    ]
 ])
 ?>
